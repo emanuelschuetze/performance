53a676278435a02da02eaf07a69a1ae852b159b7
Oskar Hahn <mail@oshahn.de>
2016-12-25 11:10:35 +0100

    Remove the flag parsing from the main func by making them global varialbes
    Also made the port flag a list of ports

--- main.go
+++ main.go
@@ -13,22 +13,93 @@ import (
 )
 
 const (
-	// The timer in milliseconds after witch the status is shown
+	// The timer in milliseconds after witch the status is shown.
 	showTimer = 100
 )
 
-func getWebsocketUrl(host string, port int, path string) string {
-	return fmt.Sprintf("ws://%s:%d%s", host, port, path)
+// The type posts is a list of ports. Its used for the command line parsing.
+// The type satisfies the flag.Value interface with the methods String()
+// and Set()
+type ports []int
+
+// Returns the ports as string.
+func (p *ports) String() string {
+	return fmt.Sprintf("%d", *p)
 }
 
-func getLoginUrl(host string, port int) string {
-	return fmt.Sprintf("http://%s:%d/users/login/", host, port)
+// Appends the value to the list of ports.
+func (p *ports) Set(value string) error {
+	tmp, err := strconv.Atoi(value)
+	if err != nil {
+		return fmt.Errorf("the value %s can not be converted to an integer.", value)
+	}
+	*p = append(*p, tmp)
+	return nil
+}
+
+// Varialbes for the flags. They are set in the init function and should not be
+// changed afterwards.
+var (
+	hostFlag      string
+	portsFlag     ports
+	projectorFlag int
+	clientsFlag   int
+	usernameFlag  string
+	passwordFlag  string
+	path          string
+)
+
+// Parse command line args
+func init() {
+	flag.StringVar(&hostFlag, "host", "localhost", "Host of OpenSlides daphne server.")
+	flag.Var(
+		&portsFlag,
+		"port",
+		"Port of OpenSlides daphne server. Multiple ports can be given to connect "+
+			"to more then one daphne server. (default 8000)")
+	flag.IntVar(
+		&projectorFlag,
+		"projector",
+		0,
+		"ID of the projector you want to connect to. Default is 0 to connect "+
+			"to site instead of projector.")
+	flag.IntVar(&clientsFlag, "clients", 500, "Number of clients that should connect to server.")
+	flag.StringVar(
+		&usernameFlag,
+		"username",
+		"",
+		"Connect with this username. Empty string for anonymous. %i is replaced by "+
+			"an number between 1 and the max count of clients.")
+	flag.StringVar(
+		&passwordFlag,
+		"password",
+		"",
+		"Use this password for the connection. %i is replaced by a number between 1 "+
+			"and max count of clients.")
+	flag.Parse()
+	if len(portsFlag) == 0 {
+		portsFlag.Set("8000")
+	}
+
+	if projectorFlag == 0 {
+		path = "/ws/site/"
+	} else {
+		path = fmt.Sprintf("/ws/projector/%d/", projectorFlag)
+	}
+}
+
+func getWebsocketUrl(port int) string {
+	return fmt.Sprintf("ws://%s:%d%s", hostFlag, port, path)
+}
+
+func getLoginUrl(port int) string {
+	return fmt.Sprintf("http://%s:%d/users/login/", hostFlag, port)
 }
 
 // Logs in with a specific username and password. Returns a session id
-func login(host string, port int, username, password string, retry int) (cookie string, err error) {
+func login(port int, username, password string, retry int) (cookie string, err error) {
 	resp, err := http.Post(
-		getLoginUrl(host, port),
+		getLoginUrl(port),
 		"application/json",
 		strings.NewReader(fmt.Sprintf(
 			"{\"username\": \"%s\", \"password\": \"%s\"}",
@@ -41,7 +112,7 @@ func login(host string, port int, username, password string, retry int) (cookie 
 
 	if resp.StatusCode >= 500 && resp.StatusCode < 600 && retry > 0 {
 		// If the error is on the server side, then retry
-		return login(host, port, username, password, retry-1)
+		return login(port, username, password, retry-1)
 	}
 
 	if resp.StatusCode != 200 {
@@ -56,10 +127,10 @@ func login(host string, port int, username, password string, retry int) (cookie 
 }
 
 // Connects to the websocket url
-func connectToWebsocket(host string, port int, path, sessionId string, receiveChannel chan bool, wsOpenChannel chan bool) {
+func connectToWebsocket(port int, sessionId string, receiveChannel chan bool, wsOpenChannel chan bool) {
 	header := make(http.Header)
 	header.Set("Cookie", "OpenSlidesSessionID="+sessionId)
-	ws, _, err := websocket.DefaultDialer.Dial(getWebsocketUrl(host, port, path), header)
+	ws, _, err := websocket.DefaultDialer.Dial(getWebsocketUrl(port), header)
 	if err != nil {
 		log.Fatal("Websocket error:", err)
 	}
@@ -78,69 +149,40 @@ func connectToWebsocket(host string, port int, path, sessionId string, receiveCh
 
 // Creates a lot of websocket connections to the server
 func main() {
-	// Parse command line args
-	host := flag.String("host", "localhost", "Host of OpenSlides daphne server")
-	port := flag.Int("port", 8000, "Port of OpenSlides daphne server")
-	projector := flag.Int(
-		"projector",
-		0,
-		"ID of the projector you want to connect to. Default is 0 to connect "+
-			"to site instead of projector.")
-	numberOfWSClients := flag.Int("clients", 500, "Number of clients that should connect to server")
-	username := flag.String(
-		"username",
-		"",
-		"Connect with this username. Empty string for anonymous. %i is replaced by "+
-			"an number between 1 and the max count of clients.")
-	password := flag.String(
-		"password",
-		"",
-		"Use this password for the connection. %i is replaced by a number between 1 "+
-			"and max count of clients.")
-	flag.Parse()
-
 	// Connect to server via websocket
-	var path, sessionId string
+	var sessionId string
 	var err error
-	if *projector == 0 {
-		path = "/ws/site/"
-	} else {
-		path = fmt.Sprintf("/ws/projector/%d/", *projector)
-	}
-	receiveChannel := make(chan bool, *numberOfWSClients)
-	wsOpenChannel := make(chan bool, *numberOfWSClients)
-	fmt.Printf("Try to connect %d clients to %s\n", *numberOfWSClients, getWebsocketUrl(*host, *port, path))
+	receiveChannel := make(chan bool, clientsFlag)
+	wsOpenChannel := make(chan bool, clientsFlag)
+	fmt.Printf("Try to connect %d clients to %s\n", clientsFlag, getWebsocketUrl(portsFlag[0]))
 
 	// Create a sessionId. If username is empty (use anonymous) then we need no
 	// login at all. If it contains the placeholder %i then we can not use a global
 	// session for all connections and have to set it individualy
-	if strings.Contains(*username, "%i") || *username == "" {
+	if strings.Contains(usernameFlag, "%i") || usernameFlag == "" {
 		sessionId = ""
 	} else {
-		sessionId, err = login(*host, *port, *username, *password, 3)
+		sessionId, err = login(portsFlag[0], usernameFlag, passwordFlag, 3)
 		if err != nil {
 			log.Fatal("Login error: ", err)
 		}
 	}
-	for i := 0; i < *numberOfWSClients; i++ {
-		go func(sessionId string, clientcount int) {
-			if sessionId == "" && *username != "" {
+	for i := 0; i < clientsFlag; i++ {
+		go func(sessionId string, clientCount int) {
+			if sessionId == "" && usernameFlag != "" {
 				// If the sessionId was not set in the lines above but the username
 				// is not empty, then we it contains the placeholder %i and we have to
 				// make the login request for each connection.
 				sessionId, err = login(
-					*host,
-					*port,
-					strings.Replace(*username, "%i", strconv.Itoa(clientcount+1), 1),
-					strings.Replace(*password, "%i", strconv.Itoa(clientcount+1), 1),
+					portsFlag[0], // TODO
+					strings.Replace(usernameFlag, "%i", strconv.Itoa(clientCount+1), 1),
+					strings.Replace(passwordFlag, "%i", strconv.Itoa(clientCount+1), 1),
 					3)
 				if err != nil {
-					// Do not exit the test if some connection fail to login. Only print
-					// the error.
-					log.Print("Login error: ", err)
+					log.Fatal("Login error: ", err)
 				}
 			}
-			connectToWebsocket(*host, *port, path, sessionId, receiveChannel, wsOpenChannel)
+			connectToWebsocket(portsFlag[0], sessionId, receiveChannel, wsOpenChannel)
 		}(sessionId, i)
 	}
 
@@ -155,7 +197,7 @@ func main() {
 		select {
 		case <-wsOpenChannel:
 			wsOpenCounter++
-			if wsOpenCounter >= *numberOfWSClients {
+			if wsOpenCounter >= clientsFlag {
 				fmt.Println("Connections established.")
 			}
 		case <-receiveChannel:
